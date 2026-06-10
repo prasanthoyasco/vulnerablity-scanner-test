f3abb5f4691e38be6728640b205729edbc768ac5
prasanthoyasco <prasanthoyasco@gmai.com>
2026-06-10 15:23:30 +0530

    new config for testing added

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiToken: "tok_live_scanner_test_111111111111111111111111",
+  password: "HardcodedPassword123!",
+  jwtSecret: "hardcoded-jwt-secret-for-regex-detection",
+  secret_key_base: "4f3c2b1a-scanner-test-secret-key-base-000000000000"
+}
